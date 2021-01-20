662f019caf2990e970f56028de57cab796f2de68
itamar <itamarlaufer1@gmail.com>
2021-01-20 12:54:25 +0200

    Add test using parmaetrize phase 1

--- tests/test_is_prime.py
+++ tests/test_is_prime.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pytest_tutorial import is_prime
 
 # hard coded multi tests
@@ -12,7 +14,7 @@ def test_3_is_prime():
 
 
 def test_43_is_prime():
-    assert is_prime(3)
+    assert is_prime(43)
 
 
 def test_1_is_not_prime():
@@ -25,3 +27,16 @@ def test_4_is_not_prime():
 
 def test_100_is_not_prime():
     assert not is_prime(100)
+
+
+# Using parametrize phase 1
+
+
+@pytest.mark.parametrize("number", (2, 3, 43))
+def test_is_prime(number):
+    assert is_prime(number)
+
+
+@pytest.mark.parametrize("number", (1, 4, 100, 101))
+def test_is_not_prime(number):
+    assert not is_prime(number)
