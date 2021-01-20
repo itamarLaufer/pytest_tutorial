df9dd29b7085e6db0900607836d6904d775f7b05
itamar <itamarlaufer1@gmail.com>
2021-01-20 12:58:51 +0200

    Add parmaterize tests phase 2

--- tests/test_is_prime.py
+++ tests/test_is_prime.py
@@ -37,6 +37,14 @@ def test_is_prime(number):
     assert is_prime(number)
 
 
-@pytest.mark.parametrize("number", (1, 4, 100, 101))
+@pytest.mark.parametrize("number", (1, 4, 100))
 def test_is_not_prime(number):
     assert not is_prime(number)
+
+
+# Using parametrize phase 2
+
+
+@pytest.mark.parametrize("number, expected_result", ((2, True), (3, True), (43, True), (1, False), (4, False), (100, False)))
+def test_is_prime(number, expected_result):
+    assert is_prime(number) == expected_result
